90a71621a4828ea69c05adc497ab6f4bc12ab44f
Steve Kondik <steve@cyngn.com>
2015-12-27 06:32:56 -0800

    video: mdss: Improve the LiveDisplay driver
     * Use a workqueue to apply changes to the hardware to reduce delays
     * Handle all update modes consistently
     * Simplify locking to prevent race conditions (seen when setting
       gamma presets)
    
    Change-Id: Ide8af437e1143a27f5b51325c27051dd97cda6b0

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -26,6 +26,7 @@
 #include "mdss_panel.h"
 #include "mdss_dsi.h"
 #include "mdss_debug.h"
+#include "mdss_livedisplay.h"
 
 static int mdss_dsi_regulator_init(struct platform_device *pdev)
 {
@@ -782,6 +783,9 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
 				mdss_dsi_set_tear_on(ctrl_pdata);
 		}
 	}
+
+	mdss_livedisplay_update(pdata->panel_info.livedisplay,
+			MODE_UPDATE_ALL);
 
 	pr_debug("%s-:\n", __func__);
 
